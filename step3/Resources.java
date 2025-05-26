6eaffa2789c5103da863ba25dca7dff737e3bae8
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-05-26 16:41:02 +0300

    remove STEP 3

--- step3/Resources.java
+++
@@ -1,7 +0,0 @@
-import java.util.concurrent.locks.ReentrantLock;
-
-public class Resources {
-    public static final Object tracker=new Object();
-    public static final ReentrantLock lock=new ReentrantLock();
-
-}
